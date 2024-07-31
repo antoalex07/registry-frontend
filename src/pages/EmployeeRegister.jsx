13e92f877267cc5442fadc57f6933edcaa1295bc
antoalex07 <antoalex710@gmail.com>
2024-07-31 13:05:51 +0530

    calendar for the register created

--- src/pages/EmployeeRegister.jsx
+++ src/pages/EmployeeRegister.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { LocalizationProvider, StaticDatePicker } from '@mui/x-date-pickers'
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
+import dayjs from 'dayjs'
+import React, { useState } from 'react'
 
 const EmployeeRegister = () => {
+
+  const [ selectedDate, setSelectedDate ] = useState(dayjs());
+  const [ employee, setEmployee ] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  }
+
   return (
-    <div>
-        <h1>Employee Register</h1>
+    <div className='employee-register'>
+        <div>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <StaticDatePicker 
+                className='calendar'
+                orientation='landscape'
+                value={selectedDate}
+                onAccept={handleDateChange}/>
+          </LocalizationProvider>
+        </div>
+        <div>
+          { employee ? (
+              <div>
+
+              </div>
+          ) : (
+              <div>
+                
+              </div>
+          )}
+        </div>
     </div>
   )
 }
