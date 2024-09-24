f20bdf0ebe9e4c64bcc8d5671160febbf3e6b997
antoalex07 <antoalex710@gmail.com>
2024-09-24 08:41:57 +0530

    loading screen setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -7,9 +7,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path='/*' element={<App/>}/>
-      </Routes>
+      <Suspense fallback={
+        
+        <div class="loader-div">
+          <span class="loader">
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+      
+      }>
+        <Routes>
+          <Route path='/*' element={<App/>}/>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>,
 )
